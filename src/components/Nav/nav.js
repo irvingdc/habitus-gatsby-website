0134ab927ff97ebf6a7e4aa72485a70c685aa27b
Irving Diaz <irvingedc@gmail.com>
2018-09-24 06:15:47 -0500

    first commit

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,7 @@
+import React from "react"
+import { Link } from "gatsby"
+import classes from "./nav.module.css"
+
+export default () =>(
+    <div className={classes.container}>Nav</div>
+)
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import classes from "./index.module.css"
+import '../main.module.css'
+import Nav from '../components/Nav/nav'
+
+export default () => (
+    <div className={classes.MainContainer}>
+        <Nav/>
+    </div>
+)
